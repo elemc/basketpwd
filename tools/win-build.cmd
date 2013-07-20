ca60080aca31e9e21344996c2c4dfeae45b88aa7
elemc <me@elemc.name>
2013-07-21 02:04:05 +0400

    Added win-build.cmd script for build in both arch

---
+++ tools/win-build.cmd
@@ -0,0 +1,19 @@
+@ECHO OFF
+mkdir build-x86
+cd build-x86
+call setenv /x86 /Release /xp
+cmake -DCMAKE_BUILD_TYPE=Release -DQT_QMAKE_EXECUTABLE=c:\develop\qt\4\x86\bin\qmake.exe -DOPENSSL_ROOT_DIR=c:\develop\openssl-x86 ..
+nmake
+nmake qt-libs
+nmake nsis
+
+cd ..
+
+mkdir build-x64
+cd build-x64
+call setenv /x64 /Release /xp
+cmake -DCMAKE_BUILD_TYPE=Release -DQT_QMAKE_EXECUTABLE=c:\develop\qt\4\x64\bin\qmake.exe -DOPENSSL_ROOT_DIR=c:\develop\openssl-x64 ..
+nmake
+nmake qt-libs
+nmake nsis
+
