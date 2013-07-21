aa7a49b193e4df23888732781e35925a39f8b870
elemc <me@elemc.name>
2013-07-21 13:25:20 +0400

    Fix arch in windows build script

--- tools/win-build.cmd
+++ tools/win-build.cmd
@@ -22,6 +22,6 @@ cd ..
 mkdir build-portable-x86
 cd build-portable-x86
 call setenv /x86 /Release /xp
-cmake -DCMAKE_BUILD_TYPE=Release -DQT_QMAKE_EXECUTABLE=c:\develop\qt\4\x64\bin\qmake.exe -DOPENSSL_ROOT_DIR=c:\develop\openssl-x64 -DPROJECT_PORTABLE=1 ..
+cmake -DCMAKE_BUILD_TYPE=Release -DQT_QMAKE_EXECUTABLE=c:\develop\qt\4\x86\bin\qmake.exe -DOPENSSL_ROOT_DIR=c:\develop\openssl-x86 -DPROJECT_PORTABLE=1 ..
 nmake
 nmake qt-libs
