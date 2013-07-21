4b38df04d158e77377797e61057ecfd1f8a38c35
elemc <me@elemc.name>
2013-07-21 13:11:09 +0400

    Added portable build in windows script

--- tools/win-build.cmd
+++ tools/win-build.cmd
@@ -17,3 +17,11 @@ nmake
 nmake qt-libs
 nmake nsis
 
+cd ..
+
+mkdir build-portable-x86
+cd build-portable-x86
+call setenv /x86 /Release /xp
+cmake -DCMAKE_BUILD_TYPE=Release -DQT_QMAKE_EXECUTABLE=c:\develop\qt\4\x64\bin\qmake.exe -DOPENSSL_ROOT_DIR=c:\develop\openssl-x64 -DPROJECT_PORTABLE=1 ..
+nmake
+nmake qt-libs
